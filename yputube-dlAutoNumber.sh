4422a4ce6cab817718050511bf24887156a93738
PraxLannister <prakharsp1502077@gmail.com>
2020-02-22 23:29:01 +0530

    Added File that includes the code to autonumber playlist while downloading from youtube-dl

---
+++ yputube-dlAutoNumber.sh
@@ -0,0 +1,2 @@
+youtube-dl -o "%(playlist_index)s-%(title)s.%(ext)s" <playlist_link>
+youtube-dl -ciot '%(autonumber)s-%(title)s.%(ext)s' 'https://www.youtube.com/playlist?list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg'
